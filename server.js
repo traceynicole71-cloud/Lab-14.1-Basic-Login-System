24505f5d10672c67c5dba1c7a17e704e5187f8e2
Tracey Roberts <traceynicole71@yahoo.com>
2026-05-20 13:43:10 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+//main auth route
+app.use('/api/users', authRoutes);
+//unhandle routes
+app.use((req, res) => res.status(404).json({ message: 'Route not found'}));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running in development mode on port ${PORT}`));
